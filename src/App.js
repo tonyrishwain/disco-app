07c875183802bda99fddbae81aafd920644800b5
tonyrishwain <tonyrishwain@Tonys-MacBook.local>
2019-07-25 16:36:20 -0700

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Column, Row } from 'simple-flexbox';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      colors: ["red", "blue", "purple", "orange"]
+    }
+  }
+  handleClick() {
+    let current = this.state.colors
+    let future = []
+    for(let i = 0; i < current.length; i++) {
+      i !== 3 ? future.push(current[i]) : future.unshift(current[i])
+    }
+    return future
+  }
+  render() {
+    return (
+      <Column flexGrow={1} onClick={() => this.setState({colors: this.handleClick()})}>
+        <Row flexGrow={1}>
+          <Column flexGrow={1} style={{backgroundColor:this.state.colors[0]}}>
+          </Column>
+          <Column flexGrow={1} style={{backgroundColor:this.state.colors[1]}}>
+          </Column>
+        </Row>
+        <Row flexGrow={1}>
+          <Column flexGrow={1} style={{backgroundColor:this.state.colors[2]}}>
+          </Column>
+          <Column flexGrow={1} style={{backgroundColor:this.state.colors[3]}}>
+          </Column>
+        </Row>
+      </Column>
+    )
+  }
 }
 
-export default App;
+export default App
